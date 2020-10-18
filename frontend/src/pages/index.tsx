5f98b30884a0c9640443bffafaf0ad561301a2fc
Julian Grande <juliangr@stud.ntnu.no>
2020-10-18 19:40:53 +0200

    Lint
    Co-authored-by: amundlrohne <r33an1@me.com>

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,28 +1,29 @@
-import { ApolloProvider, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { NextPage } from "next";
-import client from "../apollo/client";
 import { ARTICLES_QUERY } from "../apollo/query";
 import { Layout } from "../components/layout";
 import { Query } from "../types";
 
 const Home: NextPage = () => {
-    const { loading, error, data} = useQuery<Query>(ARTICLES_QUERY);
+    const { loading, error, data } = useQuery<Query>(ARTICLES_QUERY);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
 
-    return ( 
-        <div>
-            { data.articles.map((article, key) => (
-                 <div key={key}>
-                     <h1>{ article.title }</h1>
-                     <h3>Article-ID: { article.id }</h3>
-                     <p>{ article.body }</p>
-                    <span><strong>{ article.published_at }</strong></span>
+    return (
+        <Layout>
+            {data.articles.map((article, key) => (
+                <div key={key}>
+                    <h1>{article.title}</h1>
+                    <h3>Article-ID: {article.id}</h3>
+                    <p>{article.body}</p>
+                    <span>
+                        <strong>{article.published_at}</strong>
+                    </span>
                 </div>
-            )) }
-        </div>
-    )
+            ))}
+        </Layout>
+    );
 };
 
 export default Home;
